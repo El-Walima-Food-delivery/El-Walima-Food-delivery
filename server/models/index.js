5b59bbf7d2ba4d0abe6e942c5ba8f1c8efccaa55
............... .......... <Iheb>
2024-09-19 10:24:44 +0100

    stash .

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const { Sequelize,DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const connection = require("../config/database");
 const User = require("./User");
 const Restaurant = require("./Restaurant");
@@ -6,23 +6,18 @@ const MenuItem = require("./MenuItem");
 const Order = require("./Order");
 const Category = require("./category");
 
-
-const db = {}
-
-
+const db = {};
 
 db.connection = Sequelize;
 
-
 // Import models
 
-
 // Initialize models
-db.User =  User
-db.Restaurant =  Restaurant
-db.MenuItem =  MenuItem
-db.Order =  Order
-db.Category =  Category
+db.User = User;
+db.Restaurant = Restaurant;
+db.MenuItem = MenuItem;
+db.Order = Order;
+db.Category = Category;
 // connection
 //   .sync({ force: true })
 //   .then(() => {
@@ -32,4 +27,4 @@ db.Category =  Category
 //     console.error("Error syncing database:", err);
 //   });
 
-module.exports = db ;
+module.exports = db;
