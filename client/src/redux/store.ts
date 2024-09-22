26b5b7a73f7c70a29b879460b4eb2433a725ede2
daligharbi <dgharbi453@gmail.com>
2024-09-22 12:04:39 +0100

    done

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../redux/features/authSlice';
+import cartReducer from "./features/cartSlice";
 
 export const store = configureStore({
   reducer: {
     users: userSlice,
+    cart: cartReducer,
   },
 });
 
