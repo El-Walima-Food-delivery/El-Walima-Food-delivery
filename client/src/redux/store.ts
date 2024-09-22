ede3c94927a7cdb2c48154adca919a39d92124cd
daligharbi <dgharbi453@gmail.com>
2024-09-22 12:12:11 +0100

    done

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,14 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from '../redux/features/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./features/authSlice";
 import cartReducer from "./features/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    users: userSlice,
+    users: usersReducer,
     cart: cartReducer,
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
